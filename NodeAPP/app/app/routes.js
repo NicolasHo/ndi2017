310f6a8259abf1331696d7ede993f9040d119ebe
Edgar <edrflt@users.noreply.github.com>
2017-12-08 00:53:30 +0100

    mdr (#1)
    * test angular
    
    * app
    
    * ptdr

--- NodeAPP/app/app/routes.js
+++ NodeAPP/app/app/routes.js
@@ -1,6 +1,11 @@
 //	app/routes.js
 module.exports = function(app, bdd) {
 
+	app.get('/', function(req, res)
+	{
+		res.render("../views/NDI/index.ejs");
+	});
+
 	// Create alert
 	app.get('/alert_create', function(req, res) {
 		bdd.print_alertes();
