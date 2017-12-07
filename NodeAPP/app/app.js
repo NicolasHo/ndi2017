41e5cc1a0990c6a7240dd5ab3b189b8426ea9039
oligertimothee <fraxy.gfx@gmail.com>
2017-12-07 20:25:12 +0100

    init sqlite3

--- NodeAPP/app/app.js
+++ NodeAPP/app/app.js
@@ -1,8 +1,37 @@
-var express 			= require('express');
+let express 			= require('express');
+let bodyParser 		= require('body-parser');
 let expressLess   = require('express-less');
 let minifyHTML    = require('express-minify-html');
 let favicon       = require('serve-favicon');
-var app 					= express();
+let sqlite3 			= require('sqlite3').verbose();
+let app 					= express();
+
+
+let db = new sqlite3.Database('./bdd/base.sqlite3', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
+
+db.serialize(function() {
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.set('views', __dirname + '/views');
 
@@ -25,6 +54,19 @@ app.use(minifyHTML({
     minifyJS:                  true
   }
 }));
+
+app.use(bodyParser.json());
+
+
+app.get('/test', function(req,res){
+	res.render('test/test.ejs');
+});
+
+app.get("/api/", function(req, res)  {
+	console.log("long= " + req.param('long') + " , lat=" + req.param('long'));
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({ a: 1 }, null, 3));
+});
 
 //Dossier statique
 app.use('/static', express.static('public'));
