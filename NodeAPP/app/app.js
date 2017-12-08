0dadfc280df59465f9724078489ec5e2036359b7
edrflt <efouille@student.42.fr>
2017-12-08 02:31:01 +0100

    ptdr

--- NodeAPP/app/app.js
+++ NodeAPP/app/app.js
@@ -55,22 +55,19 @@ require('./app/routes.js')(app, db);
 // Traitement Erreur 404
 require('./app/404.js')(app);
 
-io.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
-        pseudo = ent.encode(pseudo);
-        socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
+io.sockets.on('connection', function (socket)
+{
+    socket.on("liste_localisations", function()
+    {
+        db.getAlert(function(rows)
+        {
+            socket.emit("liste_localisations", JSON.stringify({ alerts : rows }));
+        });
     });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-    socket.on('message', function (message) {
-        message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-
-    socket.on("oui", function(m){
+    socket.on("oui", function(m)
+    {
         socket.emit("coucou",m);
-    });
     });
 });
 
