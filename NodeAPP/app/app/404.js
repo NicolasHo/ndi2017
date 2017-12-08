8c0fc7f8f9b181b7b9d69d3c7f85669a00d6a78f
Wyvern67 <nicolas.argyriou95@gmail.com>
2017-12-08 01:28:36 +0100

    On teste une petite 404 des familles

--- NodeAPP/app/app/404.js
+++ NodeAPP/app/app/404.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
 	app.use(function(req, res, next){
 		// renvoie une erreur 404 et redirige vers /
-		res.status(404).redirect('/');
+		res.status(404).render('errors/404.ejs');
   });
 }
