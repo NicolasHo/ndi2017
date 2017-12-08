ee2ad964cd3c39d0e198f2503e7522f299de9014
Nidhal DOGGA <dogga.nidhal@gmail.com>
2017-12-08 02:54:31 +0100

    decoding the coordinates captured from the RESTful API

--- iOS APP/NDI/BGAlertsController.swift
+++ iOS APP/NDI/BGAlertsController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
-import CoreMotion
+import CoreLocation
 
 let reuseIdentifier = "reuseID"
 
 class BGAlertsViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
+    private var alerts: [String: [Any]] = ["alerts": []]
+    private var addresses: [String?] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
-        
+        let request = URLRequest(url: URL(string: "http://bachaner.fr:8081/alert_list")!)
+        let session = URLSession.shared
+        session.dataTask(with: request) { [unowned self] data, response, err in
+            guard err == nil && data != nil else {
+                // TODO: handle errors
+                return
+            }
+            do {
+                self.alerts = try JSONSerialization.jsonObject(with: data!, options: []) as! [String : [Any]]
+                for alert in self.alerts["alerts"]! {
+                    let long = (alert as! [String: Double])["longitude"]!
+                    let lat = (alert as! [String: Double])["latitude"]!
+                    let geocoder = CLGeocoder()
+                    geocoder.reverseGeocodeLocation(CLLocation(latitude: lat, longitude: long),
+                                                        completionHandler: { (placemarks, error) in
+                        self.addresses.append(placemarks!.first!.name)
+                        DispatchQueue.main.async {
+                            self.tableView.reloadSections([0], with: .automatic)
+                        }
+                    })
+                }
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,7 +63,7 @@ extension BGAlertsViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return self.addresses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,8 +71,7 @@ extension BGAlertsViewController: UITableViewDataSource, UITableViewDelegate {
             return UITableViewCell()
         }
         // Configure cell
-        cell.textLabel?.text = "AN ALERT!!!"
-        
+        cell.textLabel?.text = addresses[indexPath.row]
         return cell
     }
     
